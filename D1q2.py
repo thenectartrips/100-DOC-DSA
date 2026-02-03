615a9382f005b45aa0f304508ab3514899cb3f8d
Piyush Mani Tripathi <prashambik2004@gmail.com>
2026-02-03 10:08:47 +0530

    Initial Upload

---
+++ D1q2.py
@@ -0,0 +1,22 @@
+int* twoSum(int* nums, int numsSize, int target, int* returnSize)
+{
+    int i, j;
+    int* ans = (int*)malloc(2 * sizeof(int));
+
+    for(i = 0; i < numsSize; i++)
+    {
+        for(j = i + 1; j < numsSize; j++)
+        {
+            if(nums[i] + nums[j] == target)
+            {
+                ans[0] = i;
+                ans[1] = j;
+                *returnSize = 2;
+                return ans;
+            }
+        }
+    }
+
+    *returnSize = 0;
+    return ans;
+}
