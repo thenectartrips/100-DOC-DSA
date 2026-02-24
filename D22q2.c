44b2cc19c8d4af5d4f320affb70525d64a5970fc
Piyush Mani Tripathi <prashambik2004@gmail.com>
2026-02-24 21:19:46 +0530

    Initial Upload

---
+++ D22q2.c
@@ -0,0 +1,17 @@
+struct ListNode* reverseList(struct ListNode* head) {
+    
+    if (head == NULL || head->next == NULL) {
+        return head;
+    }
+    
+    
+    struct ListNode* newHead = reverseList(head->next);
+    
+    
+    head->next->next = head;
+    
+    
+    head->next = NULL;
+    
+    return newHead;
+}
