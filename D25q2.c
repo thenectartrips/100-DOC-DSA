5a71b6de2140ddafb2912127a1082b68edc1b793
Piyush Mani Tripathi <prashambik2004@gmail.com>
2026-02-28 15:16:45 +0530

    Initial Upload

---
+++ D25q2.c
@@ -0,0 +1,27 @@
+struct ListNode *detectCycle(struct ListNode *head) 
+{
+    if(head == NULL || head->next == NULL)
+        return NULL;
+
+    struct ListNode *slow = head;
+    struct ListNode *fast = head;
+
+    while(fast != NULL && fast->next != NULL)
+    {
+        slow = slow->next;
+        fast = fast->next->next;
+
+        if(slow == fast)
+        {
+            slow = head;
+            while(slow != fast)
+            {
+                slow = slow->next;
+                fast = fast->next;
+            }
+            return slow;
+        }
+    }
+
+    return NULL;
+}
