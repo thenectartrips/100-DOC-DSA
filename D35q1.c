4c91e2120d5b37c42960669114f6ead07149a115
Piyush Mani Tripathi <prashambik2004@gmail.com>
2026-03-31 21:45:39 +0530

    Initial Upload

---
+++ D35q1.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+
+#define SIZE 100
+
+int queue[SIZE];
+int front = -1, rear = -1;
+
+void enqueue(int value) {
+    if (rear == SIZE - 1) {
+        printf("Queue Overflow\n");
+        return;
+    }
+    if (front == -1) {
+        front = 0;
+    }
+    rear++;
+    queue[rear] = value;
+}
+
+void display() {
+    if (front == -1) {
+        printf("Queue is Empty\n");
+        return;
+    }
+    for (int i = front; i <= rear; i++) {
+        printf("%d ", queue[i]);
+    }
+}
+
+int main() {
+    int n, x;
+
+    scanf("%d", &n);
+
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &x);
+        enqueue(x);
+    }
+
+    display();
+
+    return 0;
+}
